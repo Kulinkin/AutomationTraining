32eff5e67a5d22d360d005634dad4473d065e017
Artem Kulinkin <kulinkin.a.v@gmail.com>
2021-12-16 11:53:04 +0300

    Added task 20

---
+++ AutomationCSharpTraining/AutomationCSharpTraining/HomePage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace AutomationCSharpTraining
+{
+    class HomePage
+    {
+        IWebDriver _driver;
+        const string MailLinkLocator = "desk-notif-card__login-new-item";
+
+        public IWebElement MailLink { get; set; }
+        public List<IWebElement> LiTags;
+
+        public HomePage(IWebDriver driver)
+        {
+            _driver = driver;
+            MailLink = _driver.FindElement(By.ClassName(MailLinkLocator));
+
+            /*Examples of 'By search' that are not presented in the flow of the test
+            By Name:
+            var SearchField = _driver.FindElement(By.Name("text"));
+
+            By Tag Name:
+            LiTags = _driver.FindElements(By.TagName("li")).ToList();
+
+            By Link Text:
+            var WeatherLink = _driver.FindElement(By.LinkText("Погода"));
+
+            By Partial Link Text:
+            var LinkWithCustomText = _driver.FindElement(By.PartialLinkText("ро"));
+
+            By CSS
+            var NewsLink = _driver.FindElement(By.CssSelector("span[class='news__tab-text']"));
+            */            
+        }
+
+        public void EnterMailLoginPage()
+        {
+            MailLink.Click();            
+        }
+    }
+}
---
+++ AutomationCSharpTraining/AutomationCSharpTraining/UnitTest1.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Linq;
+
+
+namespace AutomationCSharpTraining
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--incognito");
+            driver = new ChromeDriver(options);
+        }
+
+        [Test]
+        public void SuccessfullLogin()
+        {
+            
+            driver.Url = "https://yandex.by";
+            HomePage homePage = new HomePage(driver);
+            Thread.Sleep(2000);
+
+            homePage.EnterMailLoginPage();
+            Thread.Sleep(2000);
+
+            UsernamePage usernamePage = new UsernamePage(driver);
+            usernamePage.EnterUsername("iifortest");
+            Thread.Sleep(2000);
+
+            PasswordPage passwordPage = new PasswordPage(driver);
+            passwordPage.EnterPassword("1wsx@QAZ");
+
+            Thread.Sleep(5000);
+
+            MailPage mailPage = new MailPage(driver);
+            
+            Assert.IsTrue(mailPage.IsLoggedIn("ifortest"));
+
+            driver.Close();
+        }
+    }
+}
